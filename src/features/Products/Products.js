244348e88969f3fe1422d07a16592d029793f758
Lucian Tompa <lucian.tompa@ad.lacasapane.ro>
2022-08-25 02:29:42 +0300

    final-project-TompaLucian

--- src/features/Products/Products.js
+++ src/features/Products/Products.js
@@ -1,12 +1,15 @@
 import React from 'react';
 
 import { useEffect, useState } from "react";
+import { Link } from 'react-router-dom';
+import { useAuthContext } from '../Auth/AuthContext';
 import { ProductCard } from "./ProductCard";
 
 import styles from "./Products.module.css";
 
 export function Products () {
     const [products, setProducts] = useState(null);
+    const {user} = useAuthContext();
 
     useEffect(() => {
         fetch('http://localhost:3005/api/products')
@@ -20,7 +23,10 @@ export function Products () {
 
     return (
         <section className={styles['main-section']}>
-            <h1>Get in shape with our programs and products:</h1>
+            { user && (
+            <Link className={styles['add_product']} to="/Products/AddProduct">Add new program</Link> 
+            )}
+            <h1>Get in shape with our programs:</h1>
             {products.map((product) => 
             ( <ProductCard key={product.id} item={product}/>
     ))}
